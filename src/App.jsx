61132ace120f36d212107e0510550d12f5e687c4
Saif el islam Amiri <saifelislamamiri@gmail.com>
2022-06-02 11:16:00 +0100

    first project

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 
-let element = (
-  <React.Fragment>
-      <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React by doing
-        </a>
-      </header>
-    </div>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React by doing
-        </a>
-      </header>
-    </div>
-    </React.Fragment>
-)
+// let element = (
+//   <React.Fragment>
+//       <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React by doing
+//         </a>
+//       </header>
+//     </div>
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React by doing
+//         </a>
+//       </header>
+//     </div>
+//     </React.Fragment>
+// )
 
 function getTitle(){
   return <h1>{process.env.REACT_APP_TITLE}</h1>
